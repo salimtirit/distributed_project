59a53bb89c849e84cfec75a1d5253f58282103ea
Salim Kemal Tirit <salim.tirit@gmail.com>
2023-06-13 11:48:01 +0300

    bonus part chatgpt codes

---
+++ bonus_part/cube.c
@@ -0,0 +1,87 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <string.h>
+
+void handleCube(int clientSocket)
+{
+    char buffer[1024];
+    int num;
+
+    memset(buffer, 0, sizeof(buffer));
+
+    // Read the request from the client
+    if (read(clientSocket, buffer, sizeof(buffer)) > 0)
+    {
+        num = atoi(buffer);
+        int result = num * num * num;
+
+        // Send the cube result back to the client
+        snprintf(buffer, sizeof(buffer), "%d", result);
+        write(clientSocket, buffer, strlen(buffer) + 1);
+
+        printf("(cube) Request=%d\n", num);
+        printf("(cube) Reply sent as %d. Terminating...\n", result);
+    }
+
+    close(clientSocket);
+}
+
+int main()
+{
+    int port = 5020;
+
+    int serverSocket;
+    struct sockaddr_in serverAddress;
+
+    // Create socket for the server
+    serverSocket = socket(AF_INET, SOCK_STREAM, 0);
+    if (serverSocket < 0)
+    {
+        perror("Socket creation failed");
+        exit(1);
+
+        // Prepare the address structure for the server
+        serverAddress.sin_family = AF_INET;
+        serverAddress.sin_addr.s_addr = INADDR_ANY;
+        serverAddress.sin_port = htons(port);
+
+        // Bind the server socket to the specified port
+        if (bind(serverSocket, (struct sockaddr *)&serverAddress, sizeof(serverAddress)) < 0)
+        {
+            perror("Binding failed");
+            exit(1);
+        }
+
+        // Listen for connections on the port
+        listen(serverSocket, 1);
+
+        printf("(cube) cube server has started\n");
+        printf("(cube) Waiting for connections on port %d\n", port);
+
+        while (1)
+        {
+            int clientSocket;
+            struct sockaddr_in clientAddress;
+            socklen_t clientLength = sizeof(clientAddress);
+
+            // Accept incoming connections
+            clientSocket = accept(serverSocket, (struct sockaddr *)&clientAddress, &clientLength);
+            if (clientSocket < 0)
+            {
+                perror("Accept failed");
+                exit(1);
+            }
+
+            printf("(cube) Connection request received\n");
+
+            // Handle the request
+            handleCube(clientSocket);
+        }
+
+        close(serverSocket);
+
+        return 0;
+    }
---
+++ bonus_part/inetd.c
@@ -0,0 +1,103 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <string.h>
+
+void spawnServer(const char *serverName, int clientSocket);
+
+int main()
+{
+    int squarePort = 5010;
+    int cubePort = 5020;
+
+    int inetdSocket;
+    struct sockaddr_in inetdAddress;
+
+    // Create socket for the inetd server
+    inetdSocket = socket(AF_INET, SOCK_STREAM, 0);
+    if (inetdSocket < 0)
+    {
+        perror("Socket creation failed");
+        exit(1);
+    }
+
+    // Prepare the address structure for the inetd server
+    inetdAddress.sin_family = AF_INET;
+    inetdAddress.sin_addr.s_addr = INADDR_ANY;
+    inetdAddress.sin_port = 0;
+
+    // Bind the inetd socket to any available port
+    if (bind(inetdSocket, (struct sockaddr *)&inetdAddress, sizeof(inetdAddress)) < 0)
+    {
+        perror("Binding failed");
+        exit(1);
+    }
+
+    // Get the dynamically assigned port number
+    struct sockaddr_in socketAddress;
+    socklen_t addressLength = sizeof(socketAddress);
+    getsockname(inetdSocket, (struct sockaddr *)&socketAddress, &addressLength);
+    int inetdPort = ntohs(socketAddress.sin_port);
+
+    // Listen for connections on the inetd socket
+    listen(inetdSocket, 1);
+
+    printf("(inetd) inetd has started\n");
+    printf("(inetd) Waiting for ports %d & %d\n", squarePort, cubePort);
+
+    while (1)
+    {
+        int clientSocket;
+        struct sockaddr_in clientAddress;
+        socklen_t clientLength = sizeof(clientAddress);
+
+        // Accept incoming connections
+        clientSocket = accept(inetdSocket, (struct sockaddr *)&clientAddress, &clientLength);
+        if (clientSocket < 0)
+        {
+            perror("Accept failed");
+            exit(1);
+        }
+
+        printf("(inetd) Connection request received\n");
+
+        // Determine which server to spawn based on the client's requested port
+        if (clientAddress.sin_port == htons(squarePort))
+        {
+            spawnServer("square", clientSocket);
+        }
+        else if (clientAddress.sin_port == htons(cubePort))
+        {
+            spawnServer("cube", clientSocket);
+        }
+    }
+
+    close(inetdSocket);
+
+    return 0;
+}
+
+void spawnServer(const char *serverName, int clientSocket)
+{
+    pid_t pid = fork();
+
+    if (pid == 0)
+    {
+        // Child process
+        close(0);
+        dup(clientSocket);
+        close(clientSocket);
+
+        // Execute the appropriate server program
+        execl(serverName, serverName, NULL);
+
+        exit(0);
+    }
+    else if (pid < 0)
+    {
+        perror("Fork failed");
+        exit(1);
+    }
+}
